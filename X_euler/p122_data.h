f56e1ceb2f0d4217337ba4ddd91f540f046fe4c7
gramix <jeannot--m@orange.fr>
2018-06-11 17:03:35 +0200

    Add P122_data to check correct algorithm (Hansen Numbers not Brauer numbers) https://en.wikipedia.org/wiki/Addition_chain#Brauer_chain

---
+++ X_euler/p122_data.h
@@ -0,0 +1,16 @@
+//
+//  p122_data.h
+//  X_euler
+//
+//  Created by Jeannot on 11/06/2018.
+//  Copyright © 2018 Jeannot. All rights reserved.
+//
+
+#ifndef p122_data_h
+#define p122_data_h
+
+#include <stdio.h>
+#define P122_REFLG  100000
+const int32_t * P122_GetData(void) ;
+
+#endif /* p122_data_h */
