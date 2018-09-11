bb9c45ba3abd1b65f0aea5658a751e63362ae102
gramix <jeannot--m@orange.fr>
2018-09-11 22:57:55 +0200

    ajout data P022

---
+++ X_euler/p022_data.h
@@ -0,0 +1,16 @@
+//
+//  p022_data.h
+//  X_euler
+//
+//  Created by Jeannot on 11/09/2018.
+//  Copyright © 2018 Jeannot. All rights reserved.
+//
+
+#ifndef p022_data_h
+#define p022_data_h
+
+#define p022_size   5163
+const char * *  P022_GetData(void) ;
+
+
+#endif /* p022_data_h */
