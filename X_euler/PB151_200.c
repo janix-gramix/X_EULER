2eadc91c87f8767d318d528202d65fd8ff1a0f7e
gramix <jeannot--m@orange.fr>
2018-10-05 23:10:36 +0200

    version PB198e et PB198f ok

--- X_euler/PB151_200.c
+++ X_euler/PB151_200.c
@@ -14,41 +14,43 @@
 #include "faray_utils.h"
 #include "PB151_200.h"
 
-#define PB198_MAXQ  100000
+#define PB198_MAXQ  100000000
+#define PB198_MIND  100
 //#define PB198_MAXQ  1000000
 #define PB198_N     1
 #define PB198_D     9
 
 #define PB198_Nend     1
-#define PB198_Dend     100
+#define PB198_Dend     PB198_MIND
 
 
-int PB198(PB_RESULT *pbR) {
+int PB198e(PB_RESULT *pbR) {
     pbR->nbClock = clock() ;
     int64_t nbA = 0 ;
-    nbA += (PB198_MAXQ-100)/2 ; // ajout de 1/2k pour k=51,52,...,99,100
+    nbA += (PB198_MAXQ-PB198_MIND)/2 ; // ajout de 1/2k pour k=51,52,...,99,100
     int d0Max = Sqrt32(PB198_MAXQ/2) ;
     FRACTRED fr0 ;
-    nbA += (PB198_MAXQ/2 - 100) / (100*100) ;
-    for(fr0.d=101;fr0.d<=d0Max;fr0.d++) {
-        for(fr0.n=1;100*fr0.n<=fr0.d;fr0.n++) {
+    nbA += (PB198_MAXQ/2 - PB198_MIND) / (PB198_MIND*PB198_MIND) ;
+    for(fr0.d=PB198_MIND+1;fr0.d<=d0Max;fr0.d++) {
+        for(fr0.n=1;PB198_MIND*fr0.n<=fr0.d;fr0.n++) {
             FRACTRED fr1 =Besout(fr0);
             if(fr1.d*fr0.n-fr1.n*fr0.d != -1) continue ;
             int diff = PB198_MAXQ/2 - fr0.d * fr1.d ;
-            if(diff >0) {
+            {
                 int nb = diff / (fr0.d * fr0.d) ;
                 int i ;
-                for(i=1;i<=nb;i++) printf("%d/%d->%d/%d\n",fr0.n,fr0.d,fr1.n+i*fr0.n,fr1.d+i*fr0.d);
+ //               for(i=1;i<=nb;i++) printf("%d/%d->%d/%d\n",fr0.n,fr0.d,fr1.n+i*fr0.n,fr1.d+i*fr0.d);
                 nbA += nb  ;
             }
             int d = -fr1.d + fr0.d ;
             int n = -fr1.n + fr0.n ;
             diff = PB198_MAXQ/2 - fr0.d * d ;
 
-            if(diff > 0) {
+//            if(diff > 0)
+            {
                 int nb = diff / (fr0.d * fr0.d) ;
                 int i ;
-                for(i=1;i<=nb;i++) printf("%d/%d->%d/%d\n",fr0.n,fr0.d,n+i*fr0.n,d+i*fr0.d);
+ //               for(i=1;i<=nb;i++) printf("%d/%d->%d/%d\n",fr0.n,fr0.d,n+i*fr0.n,d+i*fr0.d);
                 nbA += nb  ;
             }
             
@@ -59,76 +61,63 @@ int PB198(PB_RESULT *pbR) {
     return 1 ;
 }
 
-int PB198c(PB_RESULT *pbR) {
+int PB198f(PB_RESULT *pbR) {
     pbR->nbClock = clock() ;
     int64_t nbA = 0 ;
-    nbA += (PB198_MAXQ-100)/2 ; // ajout de 1/2k pour k=51,52,...,99,100
+    nbA += (PB198_MAXQ-PB198_MIND)/2 ; // ajout de 1/2k pour k=51,52,...,99,100
     int d0Max = Sqrt32(PB198_MAXQ/2) ;
     printf("D0max=%d\n",d0Max);
-    nbA += (PB198_MAXQ/2 - 100) / (100*100) ;
+    nbA += (PB198_MAXQ/2 - PB198_MIND) / (PB198_MIND*PB198_MIND) ;
     
     SBTree *sbt = SBT_alloc() ;
     FRACTRED fr0 = {0,1} ;
-    FRACTRED fr1= {1,100} ;
+    FRACTRED fr1= {1,PB198_MIND} ;
     SBT_init(sbt,fr0,fr1) ;
     while(sbt->indS > 0  ) {
-//        if(sbt->fr0.d <=d0Max && sbt->fr1.d <=d0Max ) {
         if(sbt->fr0.d + sbt->fr1.d <=d0Max ) {
             SBT_ValidNxt(sbt,1) ;
         } else {
             if(sbt->fr0.d <=d0Max && sbt->fr0.n ) {
-                printf("%d/%d->%d/%d :",sbt->fr0.n,sbt->fr0.d,sbt->fr1.n,sbt->fr1.d);
-                int diff = PB198_MAXQ/2 - sbt->fr0.d * sbt->fr1.d ;
-                if(diff >0) {
-                    int nb = diff / (sbt->fr0.d * sbt->fr0.d) ;
-                    int i ;
-                    for(i=1;i<=nb;i++) printf("%d/%d ",sbt->fr1.n+i*sbt->fr0.n,sbt->fr1.d+i*sbt->fr0.d);
-                    nbA += nb  ;
+ //               printf("%d/%d->%d/%d :",sbt->fr0.n,sbt->fr0.d,sbt->fr1.n,sbt->fr1.d);
+                int d0 = sbt->fr0.d ;
+                int n0 = sbt->fr0.n ;
+                int d1 = sbt->fr1.d ;
+                int n1 = sbt->fr1.n ;
+                if(d0<d1) {
+                    int q = n1/n0 ;
+                    d1 -= q * d0 ;
+                    n1 -= q * n0 ;
                 }
-                printf(";") ;
-                int d = -sbt->fr1.d + sbt->fr0.d ;
-                int n = -sbt->fr1.n + sbt->fr0.n ;
-                diff = PB198_MAXQ/2 - sbt->fr0.d * d ;
+                {
+ //                   int diff = PB198_MAXQ/2 - d0 * d1 ;
+                    int diff = PB198_MAXQ/2/d0 -  d1 ;
+                    {
+//                        int nb = diff / (d0 * d0 ) ;
+                        int nb = diff /  d0  ;
+                        int i ;
+//                        for(i=1;i<=nb;i++) printf("%d/%d ",n1+i*n0,d1+i*d0);
+                        nbA += nb  ;
+                    }
+ //                   printf(";") ;
+                    d1 = -d1 + d0 ;
+                    n1 = -n1 + n0 ;
+//                    diff = PB198_MAXQ/2 - d0 * d1 ;
+                   diff = PB198_MAXQ/2/d0 - d1 ;
                 
-                if(diff > 0) {
-                    int nb = diff / (sbt->fr0.d * sbt->fr0.d) ;
-                    int i ;
-                    for(i=1;i<=nb;i++) printf("%d/%d ",n+i*sbt->fr0.n,d+i*sbt->fr0.d);
-                    nbA += nb  ;
+                    {
+//                        int nb = diff / (d0 * d0) ;
+                        int nb = diff / d0 ;
+                        int i ;
+ //                       for(i=1;i<=nb;i++) printf("%d/%d ",n1+i*n0,d1+i*d0);
+                        nbA += nb  ;
+                    }
                 }
-                printf("\n");
-                
+//                printf("\n");
             }
             SBT_ValidNxt(sbt,0) ;
         }
     }
     
- /*
-    for(fr0.d=101;fr0.d<=d0Max;fr0.d++) {
-        for(fr0.n=1;100*fr0.n<=fr0.d;fr0.n++) {
-            FRACTRED fr1 =Besout(fr0);
-            if(fr1.d*fr0.n-fr1.n*fr0.d != -1) continue ;
-            int diff = PB198_MAXQ/2 - fr0.d * fr1.d ;
-            if(diff >0) {
-                int nb = diff / (fr0.d * fr0.d) ;
-                int i ;
-                //                for(i=1;i<=nb;i++) printf("%d/%d->%d/%d\n",fr0.n,fr0.d,fr1.n+i*fr0.n,fr1.d+i*fr0.d);
-                nbA += nb  ;
-            }
-            int d = -fr1.d + fr0.d ;
-            int n = -fr1.n + fr0.n ;
-            diff = PB198_MAXQ/2 - fr0.d * d ;
-            
-            if(diff > 0) {
-                int nb = diff / (fr0.d * fr0.d) ;
-                int i ;
-                //                for(i=1;i<=nb;i++) printf("%d/%d->%d/%d\n",fr0.n,fr0.d,n+i*fr0.n,d+i*fr0.d);
-                nbA += nb  ;
-            }
-            
-        }
-    }
-  */
     pbR->nbClock = clock() - pbR->nbClock ;
     sprintf(pbR->strRes,"%lld",nbA) ;
     return 1 ;
@@ -137,24 +126,24 @@ int PB198c(PB_RESULT *pbR) {
 
 
 static inline int PB198CB(int d0,int d1) {
-    if ( d1 <= PB198_MAXQ/200  && d0*(int64_t)d1 <= PB198_MAXQ/2) return 1 ;
+    if ( d1 <= PB198_MAXQ/2*PB198_MIND  && d0*(int64_t)d1 <= PB198_MAXQ/2) return 1 ;
     else return 0 ;
 }
 
 
-/*
+
 int PB198(PB_RESULT *pbR) {
     pbR->nbClock = clock() ;
     int64_t nbA = 0 ;
-    nbA += 50+ (PB198_MAXQ)/2 - PB198_MAXQ/200 ;
+    nbA += PB198_MIND/2+ (PB198_MAXQ)/2 - PB198_MAXQ/(2*PB198_MIND) ;
 
     
     SBdTree *sbdt = SBdT_alloc() ;
-    SBdT_init(sbdt,1, 100) ;
+    SBdT_init(sbdt,1, PB198_MIND) ;
     int nbLoop = 0 ;
     while(sbdt->indS > 0 ) {
         nbLoop++ ;
-         if(sbdt->d1 <=PB198_MAXQ/200 && sbdt->d0*(int64_t)sbdt->d1 <= PB198_MAXQ/2) {
+         if(sbdt->d1 <=PB198_MAXQ/(2*PB198_MIND) && sbdt->d0*(int64_t)sbdt->d1 <= PB198_MAXQ/2) {
  //            printf("%d->%d ",sbdt->d0,sbdt->d1) ;
             nbA++ ;
             if(SBdT_ValidNxt(sbdt,1)==0) {
@@ -172,7 +161,7 @@ int PB198(PB_RESULT *pbR) {
     sprintf(pbR->strRes,"%lld",nbA-1) ;
     return 1 ;
 }
-*/
+
 
 
 
@@ -182,7 +171,7 @@ int PB198(PB_RESULT *pbR) {
 int PB198a(PB_RESULT *pbR) {
     pbR->nbClock = clock() ;
     int64_t nbA = 0 ;
-    nbA += (PB198_MAXQ-100)/2 ; //
+    nbA += (PB198_MAXQ-PB198_MIND)/2 ; //
     FRACTRED fr0 = {1,2} , fr1 = {1,1} ; ;
     
     SBTree *sbt = SBT_alloc() ;
@@ -191,7 +180,7 @@ int PB198a(PB_RESULT *pbR) {
     while(sbt->indS > 0 ) {
         nbLoop++ ;
         int64_t dd = 2 * (int64_t) sbt->fr0.n * sbt->fr1.n ;
-        int k = (int)((int64_t)(Sqrt64(dd*PB198_MAXQ+1)-sbt->fr0.n * (int64_t)sbt->fr1.d - sbt->fr1.n * (int64_t)sbt->fr0.d)/dd) - 100 +2 ;
+        int k = (int)((int64_t)(Sqrt64(dd*PB198_MAXQ+1)-sbt->fr0.n * (int64_t)sbt->fr1.d - sbt->fr1.n * (int64_t)sbt->fr0.d)/dd) - PB198_MIND +2 ;
         if(k > 0) {
             nbA += k ;
             if(SBT_ValidNxt(sbt,1)==0) {
@@ -216,7 +205,7 @@ static int loopPB198b = 0 ;
 int PB198bCB(FRACTRED fr0, FRACTRED fr1) {
     loopPB198b++ ;
     int64_t dd = 2 * (int64_t) fr0.n * fr1.n ;
-    int k = (int)((int64_t)(Sqrt64(dd*PB198_MAXQ+1)-fr0.n * (int64_t)fr1.d - fr1.n * (int64_t)fr0.d)/dd) - 100 +2 ;
+    int k = (int)((int64_t)(Sqrt64(dd*PB198_MAXQ+1)-fr0.n * (int64_t)fr1.d - fr1.n * (int64_t)fr0.d)/dd) - PB198_MIND +2 ;
     if(k>0) return k ;
     else return 0 ;
 }
@@ -224,7 +213,7 @@ int PB198bCB(FRACTRED fr0, FRACTRED fr1) {
 int PB198b(PB_RESULT *pbR) {
     pbR->nbClock = clock() ;
     int64_t nbA = 0 ;
-    nbA += (PB198_MAXQ-100)/2 ; //
+    nbA += (PB198_MAXQ-PB198_MIND)/2 ; //
     FRACTRED fr0 = {1,2} , fr1 = {1,1} ;
     nbA += STBrcv(fr0,fr1,PB198bCB) ;
     if(pbR->isVerbose) fprintf(stdout,"\tPB%s S=%lld,Version recursive (Fract[1/2,1]) loops=%d\n",pbR->ident,nbA-1,loopPB198b) ;
@@ -232,48 +221,20 @@ int PB198b(PB_RESULT *pbR) {
     sprintf(pbR->strRes,"%lld",nbA) ;
     return 1 ;
 }
-/*
-int PB198c(PB_RESULT *pbR) {
-    pbR->nbClock = clock() ;
-    int64_t nbA = 0 ;
-    nbA += (PB198_MAXQ-100)/2 ; //
-    int i ;
-    int iMax= Sqrt32(PB198_MAXQ) ;
-    SBTree *sbt = SBT_alloc() ;
-    int nbLoop = 0;
-    for(i=100;i<iMax;i++) {
-        FRACTRED fr0 = {1,i} ;
-        FRACTRED fr1= {1,i+1} ;
-        SBT_init(sbt,fr0,fr1) ;
-        while(sbt->indS > 0) {
-            nbLoop++ ;
-            if (sbt->fr0.d*(int64_t)sbt->fr1.d <= PB198_MAXQ/2) {
-//                printf("%d/%d->%d/%d\n",sbt->fr0.n,sbt->fr0.d,sbt->fr1.n,sbt->fr1.d);
-                nbA++ ;
-                SBT_ValidNxt(sbt,1) ;
-            } else {
-                SBT_ValidNxt(sbt,0) ;
-            }
-        }
-    }
-    if(pbR->isVerbose) fprintf(stdout,"\tPB%s S=%lld,Version stack(%d) Den[100 %d] loops=%d\n",pbR->ident,nbA-1,sbt->sizeStack,iMax,nbLoop) ;
-    SBT_free(sbt);
-    pbR->nbClock = clock() - pbR->nbClock ;
-    sprintf(pbR->strRes,"%lld",nbA) ;
-    return 1 ;
-}
-*/
-/*
+
+
+
+
 
 int PB198c(PB_RESULT *pbR) {
     pbR->nbClock = clock() ;
     int64_t nbA = 0 ;
-    nbA += (PB198_MAXQ-100)/2 ; //
+    nbA += (PB198_MAXQ-PB198_MIND)/2 ; //
     int i ;
     int iMax= Sqrt32(PB198_MAXQ) ;
     SBdTree *sbdt = SBdT_alloc() ;
     int nbLoop = 0;
-    for(i=100;i<iMax;i++) {
+    for(i=PB198_MIND;i<iMax;i++) {
         SBdT_init(sbdt,i,i+1) ;
         while(sbdt->indS > 0) {
             nbLoop++ ;
@@ -285,13 +246,13 @@ int PB198c(PB_RESULT *pbR) {
             }
         }
     }
-    if(pbR->isVerbose) fprintf(stdout,"\tPB%s S=%lld,Version stack(%d) Den[100 %d] loops=%d\n",pbR->ident,nbA-1,sbdt->sizeStack,iMax,nbLoop) ;
+    if(pbR->isVerbose) fprintf(stdout,"\tPB%s S=%lld,Version stack(%d) Den[%d %d] loops=%d\n",pbR->ident,nbA-1,sbdt->sizeStack,PB198_MIND,iMax,nbLoop) ;
     SBdT_free(sbdt);
     pbR->nbClock = clock() - pbR->nbClock ;
     sprintf(pbR->strRes,"%lld",nbA) ;
     return 1 ;
 }
-*/
+
 
 static int loopPB198d = 0 ;
 static inline int PB198dCB(int d0,int d) {
@@ -303,13 +264,13 @@ static inline int PB198dCB(int d0,int d) {
 int PB198d(PB_RESULT *pbR) {
     pbR->nbClock = clock() ;
     int64_t nbA = 0 ;
-    nbA += (PB198_MAXQ-100)/2 ; //
+    nbA += (PB198_MAXQ-PB198_MIND)/2 ; //
     int i ;
     int iMax= Sqrt32(PB198_MAXQ) ;
-    for(i=100;i<iMax;i++) {
+    for(i=PB198_MIND;i<iMax;i++) {
         nbA += STBrcvDen(i,i+1,PB198dCB) ;
     }
-    if(pbR->isVerbose) fprintf(stdout,"\tPB%s S=%lld,Version recursive Den[100 %d] loops=%d\n",pbR->ident,nbA-1,iMax,loopPB198d) ;
+    if(pbR->isVerbose) fprintf(stdout,"\tPB%s S=%lld,Version recursive Den[%d %d] loops=%d\n",pbR->ident,nbA-1,PB198_MIND,iMax,loopPB198d) ;
     pbR->nbClock = clock() - pbR->nbClock ;
     sprintf(pbR->strRes,"%lld",nbA) ;
     return 1 ;
