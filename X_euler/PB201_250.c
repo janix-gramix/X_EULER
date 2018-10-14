a80d49dbe57fe7fcff8d3b4b2cc1d59da7afdff9
gramix <jeannot--m@orange.fr>
2018-10-14 19:07:36 +0200

    avant nouvelle optimisation de P206

--- X_euler/PB201_250.c
+++ X_euler/PB201_250.c
@@ -13,61 +13,55 @@
 
 
 #include "PB201_250.h"
-#define PB206_NBD   8
+#define PB206_NBD   9
 int PB206(PB_RESULT *pbR) {
     pbR->nbClock = clock();
     int64_t N,N2 ;
     int i,k ;
-    int digits[PB206_NBD+1] ;
-    int64_t pow10[PB206_NBD*2+1] ;
-    for(i=0;i<PB206_NBD+1;i++) digits[i] = 0 ;
+    int64_t digits[PB206_NBD] ;
+    int64_t pow10[PB206_NBD*2-1] ;
+    int nbDtoCheck[PB206_NBD] ;
+    int maxDigit[PB206_NBD] ;
+    for(i=0;i<PB206_NBD-1;i++) {
+        digits[i] = 0 ;
+        nbDtoCheck[i] = i/2+1 ;
+        maxDigit[i]= 9 ;
+    }
+    digits[PB206_NBD-1] = 0 ;
+    nbDtoCheck[PB206_NBD-1] = PB206_NBD ;
+    maxDigit[PB206_NBD-1] = 1 ;
+    
     pow10[0]=1 ;
-    for(i=0;i<PB206_NBD*2;i++) pow10[i+1] = pow10[i]*10 ;
+    for(i=0;i<PB206_NBD*2-2;i++) pow10[i+1] = pow10[i]*10 ;
     int is=0;
     N=0 ;
-    while(is <= PB206_NBD) {
+    while(is < PB206_NBD) {
         N2 = N*N ;
-        for(k=0;2*k<=is;k++){
-            int dk = N2/pow10[2*k] % 10  ;
-            if(dk != 9-k) break ;
+        for(k=0;k<nbDtoCheck[is];k++){
+            if((N2 % 10)  != 9-k) break ;
+            N2 /= 100 ;
         }
-        if(is==PB206_NBD) {
-            for(;k<=is;k++){
-                int dk = N2/pow10[2*k] % 10  ;
-                if(dk != 9-k) break ;
-            }
-            if(k>is) break ;
-        } else if(2*k>is) {
- //           printf("%lld ",N);
-            is++ ;
-            continue ;
+        if(k==nbDtoCheck[is]) {
+            if(is==PB206_NBD-1) break ;
+            is++ ; continue ;
         }
-        if(is==PB206_NBD) {
-            if(digits[is] == 1) {
-                digits[is]=0 ;
-                N -= pow10[is] ;
-                is-- ;
-            } else {
-                digits[is]++ ;
-                N +=pow10[is] ;
-                continue ;
-            }
-        }
-        while(is>=0 && digits[is]==9) {
+        while(is>=0 && digits[is]==maxDigit[is]) {
             digits[is]=0 ;
-            N -= 9*pow10[is] ;
+            N -= maxDigit[is]*pow10[is] ;
             is-- ;
         }
         if(is< 0) break ;
         else {
             digits[is]++ ;
             N +=pow10[is] ;
-            
         }
-    
     }
-    
-    sprintf(pbR->strRes,"%d",N);
     pbR->nbClock = clock() - pbR->nbClock ;
+    if(is<PB206_NBD-1) {
+        return 0 ;
+    } else {
+        N *= 10 ;
+    }
+    sprintf(pbR->strRes,"%lld",N);
     return 1 ;
 }
