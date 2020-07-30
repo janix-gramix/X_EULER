bf91ae179da84236fb37daa3a6411b337ec30eb8
gramix <jeannot--m@orange.fr>
2020-07-30 15:59:57 +0200

    Oubli du fichier

---
+++ X_euler/pb_debug.h
@@ -0,0 +1,16 @@
+
+#ifndef PB_debug_h
+#define PB_debug_h
+
+#include <stdio.h>
+
+#include "euler_utils.h"
+
+PROTO_PB(696b) ;
+PROTO_PB(701a) ;
+PROTO_PB(701b) ;
+PROTO_PB(702) ;
+PROTO_PB(702a) ;
+PROTO_PB(702c) ;
+PROTO_PB(707) ;
+#endif
